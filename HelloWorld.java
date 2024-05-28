0ce113fd2b5ef627c4604589e13464f0ec62abec
rohitinchale <rohitinchale49758@gmail.com>
2024-05-28 14:39:27 +0530

    D1 updation

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class Main{
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+	System.out.println("Hello World");
+	System.out.println("By D1 developer");
 	}
 }
